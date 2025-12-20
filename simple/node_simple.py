7f65e6d7a90ef8f0545609771ecb57470a0a18ce
Logan Sundaram <viplogan123@gmail.com>
2025-12-19 21:03:39 -0800

    simple node done

---
+++ simple/node_simple.py
@@ -0,0 +1,7 @@
+from langgraph.graph import MessagesState
+from prompts.system_prompts import sys_msg_simple
+
+def simple_node(llm):
+    def _node(state: MessagesState):
+        return {"messages": [llm.invoke([sys_msg_simple] + state["messages"])]}
+    return _node
