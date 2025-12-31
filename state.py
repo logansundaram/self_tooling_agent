076dc8a23caf2405fed140d3acf7583da0051282
Logan Sundaram <viplogan123@gmail.com>
2025-12-31 03:41:48 -0800

    remaking the subtasks to write directly to state

--- state.py
+++ state.py
@@ -3,3 +3,4 @@ from langgraph.graph import MessagesState
 class AgentState(MessagesState):
     complexity: str
     query: str
+    #subtasks: need to have a list of in which the planner node can fill
