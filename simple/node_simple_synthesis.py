02565e29dcb3cbe911ffb0683ffa6c5b8de66f00
Logan Sundaram <viplogan123@gmail.com>
2026-01-02 17:33:56 -0800

    new syntehsis node for tavily search

---
+++ simple/node_simple_synthesis.py
@@ -0,0 +1,7 @@
+from state import AgentState
+from prompts import sys_msg_simple_synthesize
+
+def simple_node_synthesis(llm):
+    def _node(state: AgentState):
+        return {"messages": [llm.invoke([sys_msg_simple_synthesize] + state["messages"])]}
+    return _node
