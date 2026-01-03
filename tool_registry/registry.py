37b089c6167d828b9c5aa5d3ce74e1daeda16edd
Logan Sundaram <viplogan123@gmail.com>
2026-01-02 16:02:53 -0800

    working on tavily search tool

--- tool_registry/registry.py
+++ tool_registry/registry.py
@@ -1,13 +1,36 @@
 from langchain.tools import tool
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(".env.local")
+
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+
+from langchain_tavily import TavilySearch
 
 #@tool("calculator", description="Performs arithmetic calculations. Use this for any math problems.")
 #def calc(expression: str) -> str:
 #    print("tool called")
 #    return str(eval(expression))
 
-@tool("search", description="Perform an online search. Use this for any ionstance when specific info is necessary")
-def search(expression: str) -> str:
-    return expression
+tool = TavilySearch(
+    max_results=5, #may be too many
+    topic="general",
+    # include_answer=False,
+    # include_raw_content=False,
+    # include_images=False,
+    # include_image_descriptions=False,
+    # search_depth="basic",
+    # time_range="day",
+    # include_domains=None,
+    # exclude_domains=None
+    )
+
+@tool("search", description="Perform an online search. Use this for any instance when specific info is necessary")
+def search(query: str) -> str:
+    
+    return tool
 
 @tool("add", description="Performs arithmetic addition. Use this for any math problems.")
 def add(a: int, b : int) -> int:
@@ -15,17 +38,17 @@ def add(a: int, b : int) -> int:
 
 
 @tool("subtract", description="Performs arithmetic subtraction. Use this for any math problems.")
-def add(a: int, b : int) -> int:
+def subtract(a: int, b : int) -> int:
     return a + b
 
 
 @tool("multiplication", description="Performs arithmetic multiplication. Use this for any math problems.")
-def add(a: int, b : int) -> int:
+def multiplication(a: int, b : int) -> int:
     return a + b
 
 
 @tool("division", description="Performs arithmetic division. Use this for any math problems.")
-def add(a: int, b : int) -> int:
+def division(a: int, b : int) -> int:
     return a/b
 
 
